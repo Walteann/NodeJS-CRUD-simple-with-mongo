ff65d176377b21a08f4f1c94b9bf9a449aa1cebe
walteann <walteann3@gmail.com>
2019-10-29 22:32:54 -0300

    NodeJS Crud simples com MongoDb aprendido pela Rocketseat

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }
+);
+
+requireDir('./src/models');
+
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
+
